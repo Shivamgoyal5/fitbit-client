41f4d9fb309f8aaa67bf966a2e03188fd8a42c08
Shivam Goyal <159711740+Shivamgoyal5@users.noreply.github.com>
2025-05-24 21:54:07 +0530

    Update login.jsx

--- client/src/login.jsx
+++ client/src/login.jsx
@@ -40,7 +40,7 @@ function Login() {
       className="min-h-screen relative overflow-hidden flex items-center justify-center p-6"
       style={{
         backgroundImage:
-          "linear-gradient(to bottom right, rgba(79, 70, 229, 0.8), rgba(219, 39, 119, 0.8)), url('../../public/Screenshot 2025-05-24 21461.jpg')",
+          "linear-gradient(to bottom right, rgba(79, 70, 229, 0.8), rgba(219, 39, 119, 0.8)), url('../../public/Screenshot 2025-05-24 214619.png')",
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
